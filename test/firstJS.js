cd1503919a5bafaebc95c13e7d51293a08564315
yjianpen <yujianpeng4229@gmail.com>
2022-06-05 22:15:34 -0700

    edited firstJS

--- test/firstJS.js
+++ test/firstJS.js
@@ -1,3 +1,13 @@
+class WordList{
+	constructor(length,word_arr,word_test){
+		this.length=length;
+		this.word_arr=word_arr;
+		this.word_test=test;
+
+	}
+}
+
+
 console.log('1');
 setTimeout(()=>{console.log('Hello from Gavin')},0);
 console.log('2');
